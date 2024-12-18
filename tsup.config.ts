2b292aa77ad485c3da70d3cb65afbbad2c60575f
uzmoi <rizzzse@gmail.com>
2024-12-18 19:48:48 +0900

    format esm

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
   entry: ["src/*/index.ts"],
+  format: "esm",
   treeshake: "smallest",
   dts: true,
   tsconfig: "tsconfig.build.json",
