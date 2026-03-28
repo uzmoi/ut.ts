29b6f896af6c9694904c35566c7e337418f91941
uzmoi <63585970+uzmoi@users.noreply.github.com>
2026-03-28 23:48:32 +0900

    Add clamp function

--- src/ils/simple.ts
+++ src/ils/simple.ts
@@ -1,2 +1,5 @@
 export const SameValueZero = (left: unknown, right: unknown) =>
   left === left ? left === right : right !== right;
+
+export const clamp = (x: number, lower: number, upper: number): number =>
+  +(lower > upper ? x : lower > x ? lower : x > upper ? upper : x);
