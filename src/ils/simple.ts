9842b2f83733e0375b173ee94322cd1929a9c4ab
uzmoi <63585970+uzmoi@users.noreply.github.com>
2026-03-29 00:16:24 +0900

    Add modulo function

--- src/ils/simple.ts
+++ src/ils/simple.ts
@@ -3,3 +3,17 @@ export const SameValueZero = (left: unknown, right: unknown) =>
 
 export const clamp = (x: number, lower: number, upper: number): number =>
   +(lower > upper ? x : lower > x ? lower : x > upper ? upper : x);
+
+/**
+ * Performs modulo operations,
+ * but unlike the `%` operator,
+ * always returns a positive number.
+ *
+ * @example
+ * modulo(-12, 10) === 8
+ */
+export const modulo = (x: number, mod: number): number => (
+  (x %= mod) < 0
+    ? mod < 0 ? x - mod : x + mod // x + abs(mod)
+    : x + 0 // -0 + +0 === +0
+);
